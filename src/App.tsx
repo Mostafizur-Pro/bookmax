313e9b140efe5b57111983ac53422efa521f8a3e
Mostafizur-pro <mostafizur0195@gmail.com>
2023-07-14 10:16:53 +0600

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <MainLayout />
     </>
   );
 }
