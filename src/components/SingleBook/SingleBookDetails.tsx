5ce570d1cca04080167b058959323aa3513e0c4a
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-07-17 18:04:35 +0600

    single product details

--- src/components/SingleBook/SingleBookDetails.tsx
+++ src/components/SingleBook/SingleBookDetails.tsx
@@ -4,6 +4,23 @@ import { useGetSingleBooksQuery } from "../../redux/feature/book/bookApi";
 export default function SingleBookDetails() {
   const { id } = useParams();
   const { data } = useGetSingleBooksQuery({ id });
+  //   const { item } = data.data;
   console.log("single", data?.data, id);
-  return <div>hi</div>;
+  return (
+    <div className="mx-auto flex justify-center  ml-10">
+      <div className="bg-base-100 ">
+        <figure>
+          <img src={data?.data?.image_link} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {data?.data?.title}
+            <div className="badge badge-secondary">{data?.data?.genre}</div>
+          </h2>
+          <p>Year: {data?.data?.publication}</p>
+          <p>Book: {data?.data?.author}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
